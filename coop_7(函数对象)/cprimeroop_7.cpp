53ff274b98a531d3b4c56a353a1764ffdbfde129
fat storm <55908@qq.com>
2020-10-31 21:57:42 +0800

    增加了项目7

---
+++ coop_7(函数对象)/cprimeroop_7.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+using namespace std;
+
+class absInt {
+ public:
+  int operator()(int val) { return val < 0 ? -val : val; }
+};
+
+template <typename elementType>  //普通函数
+void FuncDisplayElement(const elementType& element) {
+  cout << element << ' ' << endl;
+}
+
+template <typename elementType>  //函数对象
+class DisplayElement {
+ public:
+  void operator()(const elementType& element) const { cout << element << ' '; }
+};
+
+int main() {
+  int i = -42;
+  absInt absObj;  //函数对象
+  unsigned int ui = absObj(i);
+  cout << ui << endl;
+  vector<int> a;
+  for (int n = 0; n < 10; ++n) a.push_back(n);
+  list<char> b;
+  for (char c = 'a'; c < 'k'; ++c) b.push_back(c);
+
+  // STL算法
+  for_each(a.begin(), a.end(),
+           DisplayElement<
+               int>());  // for_each算法,参数1,2为迭代范围,参数3为显示迭代的值
+  cout << endl;
+  cout << "hello 函数对象!" << endl;
+  return 0;
+}
